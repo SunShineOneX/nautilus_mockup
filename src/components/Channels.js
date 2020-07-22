0215cbbd4f1f0027b4b16edce19eb4ba856d2428
Jeff Rock <SunShineOneX@gmail.com>
2020-07-22 19:38:56 -0400

    styles upd

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -5,8 +5,15 @@ import "../App.css";
 function Channels() {
     return (
         <div class = "container">
-            <h5 class="channels-header">Channels</h5>
-                <p>TUNE IN TO NAUTILUS DEEP DIVE PORTALS</p>
+            <div class="head-container">
+            <h5 className="main-header">Channels</h5>
+            <p className="sub-header">TUNE IN TO NAUTILUS DEEP DIVE PORTALS</p>
+            </div>
+            <div class= "row">
+                <div id="card" className="card">
+
+                </div>
+            </div>
         </div>
     )
 }
