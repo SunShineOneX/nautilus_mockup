8a346388cda202ee56ef4ce90216112c3fe72f1e
Jeff Rock <SunShineOneX@gmail.com>
2020-07-22 19:41:39 -0400

    card container upd

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -4,14 +4,16 @@ import "../App.css";
 
 function Channels() {
     return (
-        <div class = "container">
+        <div class= "container">
             <div class="head-container">
             <h5 className="main-header">Channels</h5>
             <p className="sub-header">TUNE IN TO NAUTILUS DEEP DIVE PORTALS</p>
             </div>
-            <div class= "row">
-                <div id="card" className="card">
-
+            <div class="card-deck" id="card-deck">
+                <div class="row">
+                    <div class="col-lg-4">
+                        
+                    </div>
                 </div>
             </div>
         </div>
