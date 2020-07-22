d2fa8c231d7e0f9fea05515c13cc52aa11946027
Jeff Rock <SunShineOneX@gmail.com>
2020-07-22 10:17:45 -0400

    app js updates and channels.js creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
 import './App.css';
+import Channels from "./components/Channels";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Channels />
     </div>
   );
 }
