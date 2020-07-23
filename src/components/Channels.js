33857cc64cd02a1ddea12275d7f2915302283716
Jeff Rock <SunShineOneX@gmail.com>
2020-07-22 21:04:04 -0400

    more card style updates

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -15,24 +15,24 @@ function Channels() {
                         <div id="card" class="card">
                             <p>BIOLOGY+BEYOND</p>
                             <img class="card-images" src={require("../assets/bio_and_beyond.jpeg")}className="card-img-top frag" alt=""></img>
-                            <p>Making sense of the genome at last</p>
-                            <p></p>
+                            <p class="sub-title">Making Sense of the Genome at Last</p>
+                            <p class="author">by Adam Piere</p>
                         </div>
                         </div>
                     <div class="col-lg-4">
                         <div id="card" class="card">
                             <p>Women's Science & Engineering</p>
                             <img class="card-images" src={require("../assets/wise_sci_and_engineering.png")}className="card-img-top frag" alt=""></img>
-                            <p>Making sense of the genome at last</p>
-                            <p></p>
+                            <p class="sub-title">Your Brain Is On the Brink of Chaos</p>
+                            <p class="author">by Kelly Clancy</p>
                         </div>
                         </div>
                         <div class="col-lg-4">
                         <div id="card" class="card">
                             <p><strong>QUANTA</strong> ABSTRACTIONS</p>
                             <img class="card-images" src={require("../assets/abstractions.jpeg")}className="card-img-top frag" alt=""></img>
-                            <p>Making sense of the genome at last</p>
-                            <p></p>
+                            <p class="sub-title">Mathematicians Calculate How Randomness Creeps In</p>
+                            <p class="author">by Marcus Woo</p>
                         </div>
                         </div>
                     </div>
