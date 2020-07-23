f3c5bebc124b7048cc73f216958a204449970979
Jeff Rock <SunShineOneX@gmail.com>
2020-07-22 21:13:45 -0400

    adding all card rows

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -18,7 +18,7 @@ function Channels() {
                             <p class="sub-title">Making Sense of the Genome at Last</p>
                             <p class="author">by Adam Piere</p>
                         </div>
-                        </div>
+                    </div>
                     <div class="col-lg-4">
                         <div id="card" class="card">
                             <p>Women's Science & Engineering</p>
@@ -26,18 +26,96 @@ function Channels() {
                             <p class="sub-title">Your Brain Is On the Brink of Chaos</p>
                             <p class="author">by Kelly Clancy</p>
                         </div>
-                        </div>
-                        <div class="col-lg-4">
+                    </div>
+                    <div class="col-lg-4">
                         <div id="card" class="card">
                             <p><strong>QUANTA</strong> ABSTRACTIONS</p>
                             <img class="card-images" src={require("../assets/abstractions.jpeg")}className="card-img-top frag" alt=""></img>
                             <p class="sub-title">Mathematicians Calculate How Randomness Creeps In</p>
                             <p class="author">by Marcus Woo</p>
                         </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p>BIOLOGY+BEYOND</p>
+                            <img class="card-images" src={require("../assets/bio_and_beyond.jpeg")}className="card-img-top frag" alt=""></img>
+                            <p class="sub-title">Making Sense of the Genome at Last</p>
+                            <p class="author">by Adam Piere</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p>Women's Science & Engineering</p>
+                            <img class="card-images" src={require("../assets/wise_sci_and_engineering.png")}className="card-img-top frag" alt=""></img>
+                            <p class="sub-title">Your Brain Is On the Brink of Chaos</p>
+                            <p class="author">by Kelly Clancy</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p><strong>QUANTA</strong> ABSTRACTIONS</p>
+                            <img class="card-images" src={require("../assets/abstractions.jpeg")}className="card-img-top frag" alt=""></img>
+                            <p class="sub-title">Mathematicians Calculate How Randomness Creeps In</p>
+                            <p class="author">by Marcus Woo</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p>BIOLOGY+BEYOND</p>
+                            <img class="card-images" src={require("../assets/bio_and_beyond.jpeg")}className="card-img-top frag" alt=""></img>
+                            <p class="sub-title">Making Sense of the Genome at Last</p>
+                            <p class="author">by Adam Piere</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p>Women's Science & Engineering</p>
+                            <img class="card-images" src={require("../assets/wise_sci_and_engineering.png")}className="card-img-top frag" alt=""></img>
+                            <p class="sub-title">Your Brain Is On the Brink of Chaos</p>
+                            <p class="author">by Kelly Clancy</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p><strong>QUANTA</strong> ABSTRACTIONS</p>
+                            <img class="card-images" src={require("../assets/abstractions.jpeg")}className="card-img-top frag" alt=""></img>
+                            <p class="sub-title">Mathematicians Calculate How Randomness Creeps In</p>
+                            <p class="author">by Marcus Woo</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p>BIOLOGY+BEYOND</p>
+                            <img class="card-images" src={require("../assets/bio_and_beyond.jpeg")}className="card-img-top frag" alt=""></img>
+                            <p class="sub-title">Making Sense of the Genome at Last</p>
+                            <p class="author">by Adam Piere</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p>Women's Science & Engineering</p>
+                            <img class="card-images" src={require("../assets/wise_sci_and_engineering.png")}className="card-img-top frag" alt=""></img>
+                            <p class="sub-title">Your Brain Is On the Brink of Chaos</p>
+                            <p class="author">by Kelly Clancy</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p><strong>QUANTA</strong> ABSTRACTIONS</p>
+                            <img class="card-images" src={require("../assets/abstractions.jpeg")}className="card-img-top frag" alt=""></img>
+                            <p class="sub-title">Mathematicians Calculate How Randomness Creeps In</p>
+                            <p class="author">by Marcus Woo</p>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
 
