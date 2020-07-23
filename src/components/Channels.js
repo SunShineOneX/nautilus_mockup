a43e6244a0891d7357b6be9676de401e8351d6eb
Jeff Rock <SunShineOneX@gmail.com>
2020-07-22 20:49:46 -0400

    cards upd channels.js

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -14,17 +14,21 @@ function Channels() {
                     <div class="col-lg-4">
                         <div id="card" class="card">
                             <p>BIOLOGY+BEYOND</p>
+                            <img class="card-images" src={require("../assets/bio_and_beyond.jpeg")}className="card-img-top frag" alt=""></img>
+                            <p>Making sense of the genome at last</p>
+                            <p></p>
                         </div>
                         </div>
                     <div class="col-lg-4">
                         <div id="card" class="card">
                             <p>Women's Science & Engineering</p>
+                            <img class="card-images" src={require("../assets/wise_sci_and_engineering.png")}className="card-img-top frag" alt=""></img>
                         </div>
                         </div>
                         <div class="col-lg-4">
                         <div id="card" class="card">
                             <p><strong>QUANTA</strong> ABSTRACTIONS</p>
-                            <img class="images" src={require("../assets/abstractions.jpeg")}className="card-img-top frag" alt=""></img>
+                            <img class="card-images" src={require("../assets/abstractions.jpeg")}className="card-img-top frag" alt=""></img>
                         </div>
                         </div>
                     </div>
