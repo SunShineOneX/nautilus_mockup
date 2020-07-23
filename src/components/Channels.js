391d378384360387ed9dac62b9d3f663cbdc7d50
Jeff Rock <SunShineOneX@gmail.com>
2020-07-22 20:01:05 -0400

    creating top row of cards

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -13,13 +13,22 @@ function Channels() {
                 <div class="row">
                     <div class="col-lg-4">
                         <div id="card" class="card">
-                            
+                            <p>BIOLOGY+BEYOND</p>
                         </div>
-
+                        </div>
+                    <div class="col-lg-4">
+                        <div id="card" class="card">
+                            <p>Women's Science & Engineering</p>
+                        </div>
+                        </div>
+                        <div class="col-lg-4">
+                        <div id="card" class="card">
+                            Science
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
